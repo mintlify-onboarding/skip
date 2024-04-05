674aa3438ce09ba5de40f504c6b6d579beb9e914
Han Wang <44352119+handotdev@users.noreply.github.com>
2024-04-05 14:21:24 -0700

    Adding wildcard

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ module.exports = {
           source: '/skip-slinky',
           destination: 'https://skip-slinky.mintlify.app',
         },
+        {
+          source: '/skip-slinky/:path*',
+          destination: 'https://skip-slinky.mintlify.app/:path*',
+        },
       ],
     };
   },
