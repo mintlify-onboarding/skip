1737b7d09787d86553c412207509a190819dd034
Han Wang <44352119+handotdev@users.noreply.github.com>
2024-04-05 14:22:23 -0700

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return {
       beforeFiles: [
@@ -14,3 +15,5 @@ module.exports = {
     };
   },
 };
+
+export default nextConfig;
